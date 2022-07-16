dfa0f20840715871d304f0f5e72ea0fd537efe85
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-07-16 14:36:02 +0530

    Fix service-worker file access and navigation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Register Service Worker
 if('serviceWorker' in navigator) {
     console.log('INFO - Service Worker Supported')
-    navigator.serviceWorker.register('/service-worker.js').then(response => {
+    navigator.serviceWorker.register('../service-worker.js').then(response => {
         console.log('SW Register Status ->', response?.active?.state)
     }).catch(error => {
         console.log('SW Register Error ->', error)
